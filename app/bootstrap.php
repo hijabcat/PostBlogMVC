2f4674d716e5f87672a9a8068f3bd06a88515067
nik zaim <nikzaim0894@gmail.com>
2020-02-25 15:02:09 +0800

    final

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
-    // Load Libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
-?>
+//Load Config
+require_once 'config/config.php';
+// Load Helpers
+require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
+//Autoload Core libraries
+spl_autoload_register(function ($className) {
+    require_once 'libraries/' . $className . '.php';
+});
